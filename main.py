56761054ad4cee1f8cdc5c7a2829214afa97b64a
alex14-byte <nbakolcherin@gmail.com>
2022-10-18 17:02:24 +0500

    123
    123

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ mas = [input() for i in range(n)]
 for i in range(n):
     print(mas[i], sep="")
 print("   ")
-for i in range(n - 1):
-    for j in range(n - 2, i - 1, -1):
-        if mas[j + 1] < mas[j]:
-            mas[j], mas[j + 1] = mas[j + 1], mas[j]
+j = int(input())
+if (j == 1):
+    for i in range(n - 1):
+        for j in range(n - 2, i - 1, -1):
+            if mas[j + 1] < mas[j]:
+                mas[j], mas[j + 1] = mas[j + 1], mas[j]
+else:
+    for i in range(n - 1):
+        for j in range(n - 2, i - 1, -1):
+            if mas[j + 1] > mas[j]:
+                mas[j], mas[j + 1] = mas[j + 1], mas[j]
 for i in range(n):
     print(mas[i], sep="")
